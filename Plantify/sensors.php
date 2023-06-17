3709ade3d9a63a4fdcc4bf85542a3ee1810d7cd2
knas1 <83686663+knas1@users.noreply.github.com>
2023-06-17 18:02:23 +0300

    Update sensors.php

--- Plantify/sensors.php
+++ Plantify/sensors.php
@@ -12,15 +12,17 @@ if (!isset($_SESSION['logged_in'])) {
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Plantify</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="styles.css">
 	<script src="script.js"></script>
+
 </head>
 <body>
+	<h1>Plantify</h1>
 	<div class="value-set">
 		<p><strong>Humidity:</strong> <span id="1"></span><strong>%</strong></p>
 		<p><strong>Temperature:</strong> <span id="2"></span><strong>C</strong></p>
-		<p><strong>LM temp:</strong> <span id="3"></span><strong>C</strong></p>
 	</div>
 	<div class="value-set">
 		<p><strong>Soil moisture:</strong> <span id="4"></span><strong>%</strong></p>
@@ -31,7 +33,14 @@ if (!isset($_SESSION['logged_in'])) {
 		<button id="waterPump-button" class="off-button">Water pump OFF</button>
 		<button id="Fan-button" class="off-button">Fan OFF</button>
 		<button id="Light-button" class="off-button">Light OFF</button>
-
 	</div>
+	<div id="success-message" style="display: none;">Data sent successfully, water pump turned on </div>
+	<div id="success-message2" style="display: none;">Turned off</div>
+	<div id="error-message" style="display: none;">Error sending to the API</div>
+	<div id="water-tank" style="display: none;">The water tank is empty, Refill it</div>
+
+
+
+
 </body>
 </html>
